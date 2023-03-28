226a61104fedccb2f7a6bb519734e8ac36d9448e
shebli <drshebly28@gmail.com>
2023-03-28 23:44:57 +0300

    newnew

---
+++ DI-Bootcamp-2023/WEEK3/day1/xpninja.js
@@ -0,0 +1,6 @@
+//xp1
+
+let sentence = ["my","favorite","color","is","blue"];
+// Write some simple Javascript code that will join all the items in the array above, and console.log the result.
+let joinedTogether=sentence.join(' ')
+console.log(joinedTogether);
