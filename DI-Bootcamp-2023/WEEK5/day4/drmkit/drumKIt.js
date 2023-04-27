3fb0fe211dd39269f8628c85f301f09c2278ee6d
shebli <drshebly28@gmail.com>
2023-04-27 15:36:21 +0300

    new

--- DI-Bootcamp-2023/WEEK5/day4/drmkit/drumKIt.js
+++ DI-Bootcamp-2023/WEEK5/day4/drmkit/drumKIt.js
@@ -26,5 +26,14 @@ function playSound(e){
     audio.play();
 }
 
+/*
+ The code defines a div element with the class keys and a number of button elements inside it.
+  Each button element represents a drum kit component and has a data-key attribute that corresponds to 
+ a key code for the keyboard input. Additionally, each button element contains a span 
+ element that represents the name of the component, and a kbd element that displays the 
+ corresponding keyboard key.
 
+The code also includes several audio elements, each of which represents a sound file that 
+corresponds to a drum kit component. Each audio element has a src attribute that points to the
+ sound file and a data-key attribute that corresponds to the key code for the keyboard input. */
 
